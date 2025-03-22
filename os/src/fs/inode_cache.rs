2e5bf05c736d99bdcc60a08b2a1f7c30a0d40a4f
Ya0rk <345024941@qq.com>
2025-03-22 22:02:38 +0800

    实现Inode cache

--- os/src/fs/inode_cache.rs
+++ os/src/fs/inode_cache.rs
@@ -5,6 +5,7 @@
 //! corresponding inode objects. The index is thread-safe and allows for concurrent
 //! read and write operations.
 
+use core::ops::{Deref, DerefMut};
 use alloc::{
     string::{String, ToString},
     sync::Arc,
@@ -18,55 +19,45 @@ use super::InodeTrait;
 // A global, thread-safe file system index using a read-write lock.
 lazy_static! {
     /// 这个是inode cache，用hashmap结构实现，key是path，value是inode
-    pub static ref INODE_CACHE: RwLock<HashMap<String, Arc<dyn InodeTrait>>> = RwLock::new(HashMap::new());
+    pub static ref INODE_CACHE: InodeCache = InodeCache::new();
 }
 
-/// Checks if an inode exists for the given path.
-///
-/// # Arguments
-///
-/// * `path` - The path to check for an existing inode.
-///
-/// # Returns
-///
-/// `true` if the inode exists, `false` otherwise.
-pub fn has_inode(path: &str) -> bool {
-    INODE_CACHE.read().contains_key(path)
+pub struct InodeCache(pub RwLock<HashMap<String, Arc<dyn InodeTrait>>>);
+
+impl InodeCache {
+    pub fn new() -> Self {
+        Self(
+            RwLock::new(HashMap::new())
+        )
+    }
+
+    pub fn has_inode(&self, path: &str) -> bool {
+        self.read().contains_key(path)
+    }
+
+    pub fn get(&self, path: &str) -> Option<Arc<dyn InodeTrait>> {
+        self.read().get(path).map(|inode| inode.clone())
+    }
+
+    pub fn insert(&self, path: &str, inode: Arc<dyn InodeTrait>) {
+        self.write().insert(path.to_string(), inode);
+    }
+
+    pub fn remove(&self, path: &str) {
+        self.write().remove(path);
+    }
 }
 
-/// Finds the inode associated with the given path.
-///
-/// # Arguments
-///
-/// * `path` - The path to look up the inode.
-///
-/// # Returns
-///
-/// An `Option` containing the inode if it exists, or `None` if it does not.
-pub fn inode_cache_find(path: &str) -> Option<Arc<dyn InodeTrait>> {
-    INODE_CACHE.read().get(path).map(|vfile| Arc::clone(vfile))
+impl Deref for InodeCache {
+    type Target = RwLock<HashMap<String, Arc<dyn InodeTrait>>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
-/// Inserts a new inode into the file system index.
-///
-/// # Arguments
-///
-/// * `path` - The path associated with the inode.
-/// * `vfile` - The inode to insert, wrapped in an `Arc`.
-pub fn inode_cache_insert(path: &str, vfile: Arc<dyn InodeTrait>) {
-    INODE_CACHE.write().insert(path.to_string(), vfile);
-}
-
-/// Removes an inode from the file system index.
-///
-/// # Arguments
-///
-/// * `path` - The path associated with the inode to remove.
-pub fn inode_cache_remove(path: &str) {
-    INODE_CACHE.write().remove(path);
-}
-
-/// Prints the current keys in the file system index to the console.
-pub fn print_inner() {
-    println!("{:#?}", INODE_CACHE.read().keys());
+impl DerefMut for InodeCache {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
 }
