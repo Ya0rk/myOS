1919619e0b4039cd51a8b38e593b70ee4ad7f70a
JDLu2003 <1277319667@qq.com>
2025-04-22 21:43:29 +0800

    merge report,该分支可以运行，在report文件夹中查看说明

--- os/src/drivers/mod.rs
+++ os/src/drivers/mod.rs
@@ -14,7 +14,7 @@ pub use virtio_driver::*;
 use virtio_drivers::{device::console::Size, transport::{self, mmio::{MmioTransport, VirtIOHeader}, Transport}};
 use core::ptr::NonNull;
 use log::*;
-use crate::config::KERNEL_ADDR_OFFSET;
+use crate::hal::config::KERNEL_ADDR_OFFSET;
 
 
 pub type BlockDeviceImpl = VirtIoBlkDev<VirtIoHalImpl, MmioTransport<'static>>;
@@ -40,38 +40,6 @@ impl BlockDeviceImpl {
         }
         
     }
-}
-
-use alloc::vec;
-pub fn block_device_test() {
-    let block_size: usize = 512; 
-    info!("create a new BlockDeviceImpl test");
-        let VIRTIO0: usize = crate::hal::config::VIRTIO0;
-        let mut dev:VirtIoBlkDev<VirtIoHalImpl> = unsafe { VirtIoBlkDev::new(&mut *(VIRTIO0 as *mut VirtIOHeader))};
-        let mut original_data = vec![0u8; block_size];
-        let mut test_buffer = vec![0u8; block_size];
-        let test_data = [0x55; 512];  // 测试数据模式：全部填充 0x55
-        // 1. 读取原始数据
-        dev.read_block(0, &mut original_data)
-            .expect("Failed to read original data");
-
-        // 2. 写入测试数据
-        dev.write_block(0, &test_data)
-            .expect("Failed to write test data");
-
-        // 3. 读回并验证
-        dev.read_block(0, &mut test_buffer)
-            .expect("Failed to read back test data");
-
-        // 4. 验证数据
-        assert_eq!(&test_buffer[..], &test_data[..], "Read data doesn't match written data");
-
-        // 5. 恢复原始数据
-        dev.write_block(0, &original_data)
-            .expect("Failed to restore original data");
-
-        println!("Block device test passed!");
-
 }
 
 // use alloc::vec;
