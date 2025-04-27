99c35ffa6e0f9d29b5b5f2c1054a0636a6fe3ebb
JDLu2003 <1277319667@qq.com>
2025-04-27 23:30:59 +0800

    太好了,这个版本可以运行

--- os/src/drivers/virtio_driver/mod.rs
+++ os/src/drivers/virtio_driver/mod.rs
@@ -3,6 +3,7 @@ pub mod probe;
 mod pci;
 
 pub use blk::*;
+use log::info;
 use spin::Mutex;
 use virtio_drivers::{BufferDirection, Hal, PhysAddr, PAGE_SIZE};
 use core::ptr::NonNull;
@@ -62,13 +63,14 @@ unsafe impl Hal for VirtIoHalImpl {
 
     unsafe fn share(buffer: NonNull<[u8]>, _direction: BufferDirection) -> PhysAddr {
         let vaddr = buffer.as_ptr() as *mut u8 as usize;
+        info!("visit vaddr is {:#X}", vaddr);
         // Nothing to do, as the host already has access to all memory.
-        PageTable::from_token(current_user_token())
-            .translate_va(VirtAddr::from(vaddr))
-            .unwrap()
-            .0
+        // PageTable::from_token(current_user_token())
+        //     .translate_va(VirtAddr::from(vaddr))
+        //     .unwrap()
+        //     .0
         // 注意到现在采取直接映射模式,在entry中有设置
-        // vaddr
+        vaddr - 0x9000_0000_0000_0000
     }
 
     unsafe fn unshare(_paddr: PhysAddr, _buffer: NonNull<[u8]>, _direction: BufferDirection) {
