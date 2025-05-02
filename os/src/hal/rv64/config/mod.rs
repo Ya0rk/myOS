eda6c47341cab6e36b13ec9f9a4fef7d8b517264
JDLu2003 <1277319667@qq.com>
2025-05-02 12:27:35 +0800

    修改堆大小

--- os/src/hal/rv64/config/mod.rs
+++ os/src/hal/rv64/config/mod.rs
@@ -12,7 +12,7 @@ pub const MB: usize = 1024 * KB;
 // 如果是无栈协程就不会
 pub const USER_STACK_SIZE: usize = 64 * KB;
 pub const KERNEL_STACK_SIZE: usize = 64 * KB;
-pub const KERNEL_HEAP_SIZE: usize = 0x20_0000;
+pub const KERNEL_HEAP_SIZE: usize = 0x20_00000;
 pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 12;
 
