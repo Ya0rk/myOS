ddbc1515d6714a0150dcb45b9f16e3da99291443
JDLu2003 <1277319667@qq.com>
2025-06-30 15:55:01 +0800

    fix：修改 pgid 为从 1 开始编号

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -105,7 +105,7 @@ impl TaskControlBlock {
             pid: pid_handle,
 
             // Shared
-            pgid: AtomicUsize::new(0),
+            pgid: AtomicUsize::new(1),
             tgid: AtomicUsize::new(tgid),
             task_status: SpinNoIrqLock::new(TaskStatus::Ready),
             thread_group: new_shared(ThreadGroup::new()),
