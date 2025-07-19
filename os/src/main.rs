36b05d8e6cd80f10abffd5cde6b57f98450b03ac
JDLu2003 <1277319667@qq.com>
2025-07-19 23:20:39 +0800

    feat: define LruCache trait and finished a simple implement

--- os/src/main.rs
+++ os/src/main.rs
@@ -147,7 +147,7 @@ pub fn rust_main(hart_id: usize, dt_root: usize) -> ! {
             //     error!("[TEST_ADDR] {:#x}", ins);
             // }
         }
-        crate::utils::container::lru::LinkedList::<usize>::test();
+        crate::utils::container::lru::Lru::<i32, usize>::test_lru();
 
         task::init_processors();
         spawn_kernel_task(async move { task::add_initproc().await });
--- os/src/utils/container/lru.rs
+++ os/src/utils/container/lru.rs
@@ -1,6 +1,6 @@
-use core::{marker::PhantomData, ptr::NonNull};
-
 use alloc::boxed::Box;
+use core::{hash::Hash, marker::PhantomData, ptr::NonNull};
+use hashbrown::HashMap;
 
 pub struct LinkedList<T> {
     front: Link<T>,
@@ -126,6 +126,10 @@ impl<T> LinkedList<T> {
         self.len
     }
 
+    pub fn clear(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+
     pub fn test() {
         let mut list = LinkedList::new();
 
@@ -232,5 +236,222 @@ impl<T> LinkedList<T> {
 impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
         while self.pop_front().is_some() {}
+    }
+}
+
+pub trait LruCache<K, V> {
+    /// a cheap function to get the capacity of the cache
+    fn capacity(&self) -> usize;
+
+    /// a cheap function to get the length of the cache
+    fn len(&self) -> usize;
+
+    /// a cheap function to check if the key is in the cache
+    fn contains(&self, key: &K) -> bool;
+
+    /// get a reference to the value of the key
+    ///
+    /// this function will not update the lru list
+    fn get(&self, key: &K) -> Option<&V>;
+
+    /// access the key, and update the lru list
+    ///
+    /// NOTE: this function's signature is changed to `&mut self` to allow mutation
+    fn access(&mut self, key: &K) -> bool;
+
+    /// insert a key-value pair into the cache
+    ///
+    /// if the key is already in the cache, update the value and return the old value
+    ///
+    /// if the key is not in the cache, insert the new key-value pair
+    ///
+    /// if the cache is full, remove the least recently used item
+    fn insert(&mut self, key: K, value: V) -> Option<V>;
+
+    /// remove the key-value pair from the cache
+    fn remove(&mut self, key: &K) -> Option<V>;
+
+    /// pop the least recently used item from the cache
+    fn pop(&mut self) -> Option<V>;
+
+    /// clear the cache
+    fn clear(&mut self);
+}
+
+/// A simple LRU cache implementation
+///
+/// using a HashMap and a doubly linked list
+pub struct Lru<K, V> {
+    /// the linked list is used to store the key-value pairs
+    ///
+    /// the front of the list is the most recently used item
+    ///
+    /// the back of the list is the least recently used item
+    ///
+    /// we store `(K, V)` in the list to get the key when we pop from the back
+    list: LinkedList<(K, V)>,
+
+    /// the hash map is used to store the key and a pointer to the node in the linked list
+    map: HashMap<K, NonNull<Node<(K, V)>>>,
+
+    /// the capacity of the cache
+    capacity: usize,
+}
+
+impl<K: Eq + Hash, V> Lru<K, V> {
+    pub fn new(capacity: usize) -> Self {
+        assert!(capacity > 0, "capacity must be greater than 0");
+        Self {
+            list: LinkedList::new(),
+            map: HashMap::with_capacity(capacity),
+            capacity,
+        }
+    }
+}
+
+impl<K: Eq + Hash + Clone, V> LruCache<K, V> for Lru<K, V> {
+    fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    fn len(&self) -> usize {
+        self.list.len()
+    }
+
+    fn contains(&self, key: &K) -> bool {
+        self.map.contains_key(key)
+    }
+
+    fn get(&self, key: &K) -> Option<&V> {
+        self.map
+            .get(key)
+            .map(|node| unsafe { &(*node.as_ptr()).elem.1 })
+    }
+
+    fn access(&mut self, key: &K) -> bool {
+        if let Some(node_ptr) = self.map.get(key).copied() {
+            // remove the node from the list
+            let (key, value) = self.list.remove_by_ptr(node_ptr);
+            // push it to the front
+            self.list.push_front((key, value));
+            // update the pointer in the map
+            if let Some(front) = self.list.front {
+                let key_ref = unsafe { &(*front.as_ptr()).elem.0 };
+                *self.map.get_mut(key_ref).unwrap() = front;
+            }
+            true
+        } else {
+            false
+        }
+    }
+
+    fn insert(&mut self, key: K, value: V) -> Option<V> {
+        // if the key is already in the cache, update the value
+        if let Some(node_ptr) = self.map.get(&key).copied() {
+            let (old_key, old_value) = self.list.remove_by_ptr(node_ptr);
+            self.list.push_front((old_key, value));
+            if let Some(front) = self.list.front {
+                *self.map.get_mut(&key).unwrap() = front;
+            }
+            return Some(old_value);
+        }
+
+        // if the cache is full, remove the least recently used item
+        if self.list.len() >= self.capacity {
+            if let Some((key, _)) = self.list.pop_back() {
+                self.map.remove(&key);
+            }
+        }
+
+        // insert the new key-value pair
+        self.list.push_front((key.clone(), value));
+        if let Some(front) = self.list.front {
+            self.map.insert(key, front);
+        }
+
+        None
+    }
+
+    fn remove(&mut self, key: &K) -> Option<V> {
+        self.map.remove(key).map(|node_ptr| {
+            let (_, value) = self.list.remove_by_ptr(node_ptr);
+            value
+        })
+    }
+
+    fn pop(&mut self) -> Option<V> {
+        self.list.pop_back().map(|(key, value)| {
+            self.map.remove(&key);
+            value
+        })
+    }
+
+    fn clear(&mut self) {
+        self.list.clear();
+        self.map.clear();
+    }
+}
+
+impl<K: Eq + Hash + Clone, V> Lru<K, V> {
+    /// a test function to test the lru cache
+    pub fn test_lru() {
+        println!("\n--- Testing Lru ---");
+
+        let mut lru = Lru::new(3);
+
+        // Test insertion
+        assert_eq!(lru.insert(1, "a"), None); // [1:a]
+        assert_eq!(lru.insert(2, "b"), None); // [2:b, 1:a]
+        assert_eq!(lru.insert(3, "c"), None); // [3:c, 2:b, 1:a]
+        assert_eq!(lru.len(), 3);
+        println!("Test insert: OK");
+
+        // Test get
+        assert_eq!(lru.get(&1), Some(&"a"));
+        assert_eq!(lru.get(&2), Some(&"b"));
+        assert_eq!(lru.get(&3), Some(&"c"));
+        println!("Test get: OK");
+
+        // Test insertion that causes eviction
+        assert_eq!(lru.insert(4, "d"), None); // [4:d, 3:c, 2:b], evicts 1:a
+        assert_eq!(lru.len(), 3);
+        assert_eq!(lru.contains(&1), false);
+        assert_eq!(lru.get(&4), Some(&"d"));
+        println!("Test eviction: OK");
+
+        // Test access
+        lru.access(&2); // [2:b, 4:d, 3:c]
+                        // Now inserting should evict 3
+        assert_eq!(lru.insert(5, "e"), None); // [5:e, 2:b, 4:d], evicts 3:c
+        assert_eq!(lru.contains(&3), false);
+        assert_eq!(lru.get(&5), Some(&"e"));
+        println!("Test access: OK");
+
+        // Test update
+        assert_eq!(lru.insert(2, "B"), Some("b")); // [2:B, 5:e, 4:d]
+        assert_eq!(lru.get(&2), Some(&"B"));
+        assert_eq!(lru.len(), 3);
+        println!("Test update: OK");
+
+        // Test remove
+        assert_eq!(lru.remove(&5), Some("e")); // [2:B, 4:d]
+        assert_eq!(lru.len(), 2);
+        assert_eq!(lru.contains(&5), false);
+        println!("Test remove: OK");
+
+        // Test pop (LRU element)
+        assert_eq!(lru.pop(), Some("d")); // [2:B], pops 4:d
+        assert_eq!(lru.len(), 1);
+        assert_eq!(lru.contains(&4), false);
+        println!("Test pop: OK");
+
+        // Test clear
+        lru.clear();
+        assert_eq!(lru.len(), 0);
+        assert!(lru.map.is_empty());
+        assert!(lru.list.front.is_none());
+        println!("Test clear: OK");
+
+        println!("--- All Lru tests passed! ---");
     }
 }
